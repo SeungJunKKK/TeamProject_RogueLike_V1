178e654765638dc108db21cd7565e93e638b54f5
RiamIm <167727018+RiamIm@users.noreply.github.com>
2026-07-05 12:44:33 +0900

    PoolManager 풀링 시스템 및 IPoolable 규약 구현

--- Assets/Scripts/Managers/PoolManager.cs
+++ Assets/Scripts/Managers/PoolManager.cs
@@ -1,16 +1,48 @@
+using System.Collections.Generic;
 using UnityEngine;
 
-public class PoolManager : MonoBehaviour
+public class PoolManager : Singleton<PoolManager>
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private Dictionary<GameObject, Queue<GameObject>> m_Pools = new Dictionary<GameObject, Queue<GameObject>>();
+
+    public GameObject Get(GameObject prefab, Vector3 pos, Quaternion rot)
     {
-        
+        if (!m_Pools.ContainsKey(prefab))
+            m_Pools[prefab] = new Queue<GameObject>();
+
+        GameObject obj;
+        if (m_Pools[prefab].Count > 0)
+            obj = m_Pools[prefab].Dequeue();
+        else
+        {
+            obj = Instantiate(prefab);
+            obj.AddComponent<PooledObject>().Init(prefab);
+        }
+
+
+        obj.transform.SetPositionAndRotation(pos, rot);
+        obj.SetActive(true);
+
+
+        obj.GetComponent<IPoolable>()?.OnSpawn();
+
+        return obj;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Return(GameObject prefab, GameObject obj)
     {
-        
+        obj.GetComponent<IPoolable>()?.OnDespawn();
+        obj.SetActive(false);
+        m_Pools[prefab].Enqueue(obj);
+    }
+
+    public void ClearAll()
+    {
+        foreach (var q in m_Pools.Values)
+        {
+            while (q.Count > 0)
+                Destroy(q.Dequeue());
+        }
+        m_Pools.Clear();
     }
 }
