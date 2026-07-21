c00aa9940e07dc60bc3e6bc0218b7bd14c911d95
SeungJunKKK <jihoun105@gmail.com>
2026-07-21 20:37:32 +0900

    feature : Player_Ultimate_Skill
    SuppressiveFire -> Both Mode , Single Mode
    SuppressiveBarrage(Ancient Scepter Item)
    -> Both Mode , Single Mode

---
+++ Assets/Scripts/Player/Commando/SuppressiveFireState.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+
+namespace Player.Commando
+{
+    public class SuppressiveFireState : PlayerAttackState
+    {
+        private int m_CurrentShotCount = 0;
+        private bool m_IsSingleMode; 
+
+        public SuppressiveFireState(PlayerController player, float duration)
+            : base(player, "", duration, SkillType.Ultimate_V) { }
+
+        public override void Enter()
+        {
+            m_Player.Rb.linearVelocity = Vector2.zero;
+            m_Player.CooldownManager.UseSkill(m_SkillType);
+
+            m_IsSingleMode = m_Player.MovementInput.x != 0;
+
+            if (m_IsSingleMode)
+            {
+                float dir = Mathf.Sign(m_Player.MovementInput.x);
+                m_Player.transform.rotation = Quaternion.Euler(0, dir > 0 ? 0 : 180, 0);
+
+                m_Player.Anim.Play("SuppressiveFire_Single");
+            }
+            else
+            {
+                m_Player.Anim.Play("SuppressiveFire_Both"); 
+            }
+        }
+
+        protected override void ExecuteShoot()
+        {
+            m_CurrentShotCount++;
+            Vector2 shootDirection;
+            Vector2 shootOrigin;
+
+            if (m_IsSingleMode)
+            {
+                shootDirection = m_Player.transform.right;
+
+                shootOrigin = m_Player.MuzzlePos != null
+                              ? (Vector2)m_Player.MuzzlePos.position
+                              : (Vector2)m_Player.transform.position + new Vector2(shootDirection.x * 0.5f, 0.2f);
+            }
+            else
+            {
+                float dir = (m_CurrentShotCount % 2 == 1) ? 1f : -1f;
+                shootDirection = new Vector2(dir, 0f);
+
+                if (m_Player.MuzzlePos != null)
+                {
+                    Vector3 localPos = m_Player.MuzzlePos.localPosition;
+                    Vector3 mirroredOffset = new Vector3(Mathf.Abs(localPos.x) * dir, localPos.y, localPos.z);
+                    shootOrigin = m_Player.transform.position + mirroredOffset;
+                }
+                else
+                {
+                    shootOrigin = (Vector2)m_Player.transform.position + new Vector2(dir * 0.5f, 0.2f);
+                }
+            }
+
+            float attackRange = 15f;
+            RaycastHit2D[] hits = Physics2D.RaycastAll(shootOrigin, shootDirection, attackRange);
+            bool hitSomething = false;
+
+            foreach (RaycastHit2D hit in hits)
+            {
+                TestDummyHealth dummy = hit.collider.GetComponent<TestDummyHealth>();
+                if (dummy != null)
+                {
+                    dummy.TakeDamage(10f, shootDirection, 10f);
+                    hitSomething = true;
+                }
+            }
+
+            Vector2 vfxPosition = shootOrigin + (shootDirection * (attackRange / 2f));
+            Quaternion vfxRotation = Quaternion.Euler(0, shootDirection.x > 0 ? 0 : 180, 0);
+
+            if (m_Player.SuppressiveFireVFXPrefab != null)
+            {
+                EventBus.Publish(new SpawnVFXEvent
+                {
+                    VFXPrefab = m_Player.SuppressiveFireVFXPrefab,
+                    Position = vfxPosition,
+                    Rotation = vfxRotation
+                });
+            }
+
+            if (hitSomething) m_Player.TriggerHitFeedback(shootDirection, 0.4f, 0.02f);
+            else m_Player.TriggerHitFeedback(shootDirection, 0.2f, 0f);
+        }
+    }
+}
